cd8c8de380b5cdb0d4f1a4d7abf7eb1a86f498e3
Cameron Eldridge <cameldridge@gmail.com>
2024-12-02 17:52:04 -0500

    finish votecaster looks

--- src/routes/[year]/VoteCaster.svelte
+++ src/routes/[year]/VoteCaster.svelte
@@ -3,6 +3,7 @@
   import { SvelteMap } from "svelte/reactivity";
   import CookieInfo from "./CookieInfo.svelte";
   import Icon from "$lib/components/Icon.svelte";
+  import Button from "$lib/components/Button.svelte";
 
   const { cookies }: { cookies: Cookie[] } = $props();
 
@@ -16,38 +17,79 @@
 
 <dialog bind:this={dialog}>
   <form action="?/vote" method="POST">
-    {#each ordered as cookie, i (cookie.id)}
-      <div class="cookie" draggable>
-        <div class="handle">
-          <p class="counter">{i + 1}</p>
-          <Icon>drag_handle</Icon>
+    <header>
+      <h1>Cast Your Votes</h1>
+    </header>
+    <div>
+      {#each ordered as cookie, i (cookie.id)}
+        <div class="cookie" draggable>
+          <div class="handle">
+            <p class="counter">{i + 1}</p>
+            <Icon>drag_handle</Icon>
+          </div>
+          <picture>
+            <img
+              src={cookie.image_url}
+              alt="Photo of &ldquo;{cookie.name}&rdquo;"
+              height="64"
+              width="64"
+            />
+          </picture>
+          <div class="content">
+            <p class="title">
+              {cookie.name}
+            </p>
+            <textarea name="comment[{cookie.id}]" rows="2" placeholder="Leave your review" cols="0"
+            ></textarea>
+          </div>
         </div>
-        <picture>
-          <img
-            src={cookie.image_url}
-            alt="Photo of &ldquo;{cookie.name}&rdquo;"
-            height="64"
-            width="64"
-          />
-        </picture>
-        <div class="content">
-          <p class="title">
-            {cookie.name}
-          </p>
-          <textarea name="comment[{cookie.id}]" rows="2" placeholder="Leave your review"></textarea>
-        </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
+
+    <div class="action">
+      <Button>Submit</Button>
+    </div>
   </form>
 </dialog>
 
 <style>
+  dialog {
+    margin: 2rem auto;
+    max-height: calc(100vh - 4rem);
+    padding: 2rem 0.5rem;
+
+    border: none;
+    box-shadow: 0.25rem 0.25rem 0.75rem rgb(0 0 0 / 0.25);
+    background-color: white;
+    background-image: url("/paper.png");
+    overflow-y: auto;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .action {
+    margin: 0 auto;
+  }
+
+  @media (min-width: 400px) {
+    dialog {
+      margin: 8rem auto;
+      max-height: calc(100vh - 16rem);
+      padding: 4rem 2rem;
+    }
+  }
+
   .cookie {
     cursor: grab;
-    padding: 1rem;
+    padding: 1rem 0;
     display: flex;
     flex-direction: row;
     align-items: flex-start;
+    flex-wrap: wrap;
     gap: 0.5rem;
   }
 
@@ -69,6 +111,7 @@
     justify-content: space-around;
     height: 4rem;
     color: rgb(0 0 0 / 0.5);
+    flex-shrink: 0;
   }
 
   .cookie:hover:not(:has(textarea:hover)) .handle {
@@ -80,6 +123,7 @@
     display: flex;
     flex-direction: column;
     gap: 0.25rem;
+    flex-basis: 200px;
   }
 
   .cookie:active {
@@ -92,6 +136,7 @@
     height: 4rem;
     background-color: rgb(0 0 0 / 0.05);
     overflow: hidden;
+    flex-shrink: 0;
   }
 
   textarea {
@@ -99,5 +144,14 @@
     border: 1px solid rgb(0 0 0 / 0.12);
     padding: 0.5rem;
     resize: vertical;
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-family: var(--font-display);
+    font-style: italic;
+    text-align: center;
+    text-transform: lowercase;
   }
 </style>
