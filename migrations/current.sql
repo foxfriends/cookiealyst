f651d25df7b04dd03648965a72a9a665b7a22e48
Cameron Eldridge <cameldridge@gmail.com>
2024-12-04 23:16:28 -0500

    add constraints for my own safety

--- migrations/current.sql
+++ migrations/current.sql
@@ -1,1 +1,6 @@
--- Enter migration here
+ALTER TABLE reviews DROP CONSTRAINT IF EXISTS comment_max_length;
+ALTER TABLE comments DROP CONSTRAINT IF EXISTS comment_max_length;
+ALTER TABLE accounts DROP CONSTRAINT IF EXISTS account_id_max_length;
+ALTER TABLE reviews ADD CONSTRAINT comment_max_length CHECK (length(comment) <= 2048);
+ALTER TABLE comments ADD CONSTRAINT comment_max_length CHECK (length(comment) <= 2048);
+ALTER TABLE accounts ADD CONSTRAINT account_id_max_length CHECK (length(id) <= 128);
